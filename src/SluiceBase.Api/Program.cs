8cc7f8d97c6210ac240a82c564ebb024664c483f
Yeong Jong Lim <yeongjong.lim@gmail.com>
2026-05-03 09:58:50 +0800

    feat(api): wire composition root with auth, EF, OpenAPI, endpoints

--- src/SluiceBase.Api/Program.cs
+++ src/SluiceBase.Api/Program.cs
@@ -1,10 +1,45 @@
+using Microsoft.AspNetCore.DataProtection;
+using Microsoft.EntityFrameworkCore;
+using SluiceBase.Api.Auth;
+using SluiceBase.Api.Data;
+using SluiceBase.Api.Endpoints;
+using SluiceBase.Api.Targets;
+using SluiceBase.Core.Targets;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.AddServiceDefaults();
 
+builder.AddNpgsqlDbContext<AppDbContext>("Metadata");
+
+builder.Services.AddDataProtection()
+    .PersistKeysToDbContext<AppDbContext>();
+
+builder.Services.AddSluiceBaseAuth(builder.Configuration);
+
+builder.Services.AddAntiforgery(o => o.HeaderName = "X-XSRF-TOKEN");
+
+builder.Services.AddOpenApi();
+
+builder.Services.AddSingleton<ITargetEngine, PostgresTargetEngine>();
+
 var app = builder.Build();
 
+if (app.Environment.IsDevelopment()
+    && builder.Configuration.GetValue("Migrations:AutoApply", false))
+{
+    using var scope = app.Services.CreateScope();
+    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    await db.Database.MigrateAsync();
+}
+
+app.UseAuthentication();
+app.UseAuthorization();
+app.UseAntiforgery();
+
+app.MapOpenApi();
 app.MapDefaultEndpoints();
+app.MapAllEndpoints();
 
 app.Run();
 
