b0414532e3fb1ea9ab9345b0374c6c21f1e4abd1
Yeong Jong Lim <yeongjong.lim@gmail.com>
2026-05-17 22:01:27 +0800

    chore: update Aspire to 13.3.3 and revert Keycloak endpoint workaround

--- src/AppHost/Program.cs
+++ src/AppHost/Program.cs
@@ -31,17 +31,11 @@ var greenDbInstance = builder.AddPostgres("target-green-pg")
 // Add the default database to the application model so that it can be referenced by other resources.
 var greenDb = greenDbInstance.AddDatabase("green-appdb", appDbName);
 
-var keycloak = builder.AddKeycloak("keycloak", 63330)
+var keycloak = builder.AddKeycloak("keycloak")
     .WithRealmImport("seed/keycloak")
     .WithOtlpExporter()
     .WaitFor(keycloakDb)
-    .WithPostgres(keycloakDb)
-    .WithEndpoint("https", ep =>
-    {
-        ep.Port = 63331;
-        ep.TargetPort = 8080;
-        ep.UriScheme = "https";
-    });
+    .WithPostgres(keycloakDb);
 
 var api = builder.AddProject<Projects.SluiceBase_Api>("api")
     .WithReference(metadataDb, "Metadata").WaitFor(metadataDb)
