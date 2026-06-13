451d01d4a909ba0243fbc1844942fb4b46ec984d
Yeong Jong Lim <yeongjong.lim@gmail.com>
2026-06-13 17:10:20 +0800

    Replace Permission and Groups nav items with single Access item

--- src/frontend/src/routes/_authed.tsx
+++ src/frontend/src/routes/_authed.tsx
@@ -19,12 +19,10 @@ import {
   IconLogout,
   IconMoon,
   IconServer,
-  IconShieldLock,
   IconSitemap,
   IconSparkles,
   IconSun,
   IconTerminal2,
-  IconUsers,
 } from "@tabler/icons-react";
 import { Link, Outlet, createFileRoute, useLocation } from "@tanstack/react-router";
 import { useState } from "react";
@@ -45,7 +43,6 @@ function AuthedLayout() {
   const { colorScheme, toggleColorScheme } = useMantineColorScheme();
   const location = useLocation();
   const isAdmin = useHasPermission("permission:manage");
-  const isGroupAdmin = useHasPermission("group:manage");
   const isServerAdmin = useHasPermission("server:manage");
   const canQuery = useHasPermission("query:execute");
   const canSubmitUpdates = useHasPermission("update:submit");
@@ -192,26 +189,6 @@ function AuthedLayout() {
               component={Link}
               to="/server"
               active={location.pathname === "/server"}
-              onClick={closeMobileNav}
-            />
-          )}
-          {isGroupAdmin && (
-            <NavLink
-              label="Groups"
-              leftSection={<IconUsers size={16} />}
-              component={Link}
-              to="/group"
-              active={location.pathname === "/group"}
-              onClick={closeMobileNav}
-            />
-          )}
-          {isAdmin && (
-            <NavLink
-              label="Permission"
-              leftSection={<IconShieldLock size={16} />}
-              component={Link}
-              to="/permission"
-              active={location.pathname === "/permission"}
               onClick={closeMobileNav}
             />
           )}
